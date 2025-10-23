9d6828b84f58bbc2c74c0f7115b2a50f0c54d04e
sidi-cnm <sdeiebeidy@gmail.com>
2025-10-23 17:00:26 +0000

    New model version 2

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ app = FastAPI(title="Medical Doc Pipeline API", version="0.1.0")
 
 @app.get("/health")
 def health():
-    print("Health check endpoint calledss")
+    print("Health check endpoint calleds2")
     return {"status": "ok"}
